87f9e4bd9a244cd513c071d60e3837b51476e465
jai-max <kadyanaryan744@gmail.com>
2025-04-27 02:29:01 +0530

    added logo, images and other assests with header work

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom"
 
-const Header = () => {
+export const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-2">
+    <div className="container mx-auto flex h-16 items-center justify-between px-4">
+      <Link to={"/"}>
+        <img
+          src="/assets/svgs/MockWise.png"
+          alt="MockWise logo"
+          className="h-14"
+        />
+      </Link>
 
-export default Header
+        <div className="flex gap-4"></div>
+      </div>
+    </header>
+    )
+  }
+ 
