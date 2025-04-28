70e1c3dfd74da85a9f3d4b7ae74ca1388f0868e3
jai-max <kadyanaryan744@gmail.com>
2025-04-28 16:57:36 +0530

    navigation-routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,48 @@
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"; 
 import { useAuth } from "@clerk/clerk-react";
 import { Container } from "./Container";
 import { LogoContainer } from "./logo-container";
 import { NavigationRoutes } from "./navigation-routes";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  const { userId } = useAuth()
+  const { userId } = useAuth();
 
   return (
-    <header
-      className={cn("w-full border-b duration-150 transition-all ease-in-out")}
+    <header className={cn("w-full border-b duration-150 transition-all ease-in-out")}
     >
       <Container>
-        <div className="flex items-center gap-4 w-full">
+        <div className="pl-0 flex items-center gap-8 w-full h-20">
           {/* logo section */}
           <LogoContainer />
+
           {/* navigation section */}
-         <NavigationRoutes/>
+          <nav className="hidden md:flex items-center gap-6">
+            <NavigationRoutes />
+            {userId && (
+              <NavLink 
+              to={"/generate"}
+              className={({ isActive }) =>
+                cn(
+                  "text-base text-neutral-500",
+                  isActive && "text-neutral-800 font-semibold"
+                )
+                }
+                >
+                  Take an Interview
+                </NavLink>
+            )}
+          </nav>
 
           {/* profile section */}
+          <div className="ml-auto flex items-center gap-6">
+            {/* profile section */}
 
+          </div>
         </div>
       </Container>
     </header>
+  );
+};
 
-  )
-}
-
-export default Header
+export default Header;
