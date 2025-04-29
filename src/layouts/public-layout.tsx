574614760c0e63183747b78b49c998699912f262
jai-max <kadyanaryan744@gmail.com>
2025-04-29 23:59:43 +0530

    auth handler

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/Footer"
 import Header from "@/components/Header"
+import AuthHandler from "@/handlers/auth-handler"
 import { Outlet } from "react-router-dom"
 
 export const PublicLayout = () => {
