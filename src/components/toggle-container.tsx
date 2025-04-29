b9a56b439b51155d7731e17316e74c31b5198d9a
jai-max <kadyanaryan744@gmail.com>
2025-04-29 12:00:49 +0530

    header(toggle-container)

--- src/components/toggle-container.tsx
+++ src/components/toggle-container.tsx
@@ -1,10 +1,46 @@
-
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Menu } from "lucide-react"
+import { NavigationRoutes } from "./navigation-routes"
+import { NavLink } from "react-router-dom"
+import { cn } from "@/lib/utils"
+import { useAuth } from "@clerk/clerk-react"
 
 export const ToggleContainer = () => {
+  const { userId } = useAuth();
   return (
-    <div>
-      
-    </div>
+    <Sheet>
+      <SheetTrigger className="block md:hidden">
+        <Menu/>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle/>
+        </SheetHeader>
+
+      <nav className="gap-8 flex flex-col items-start">
+        <NavigationRoutes isMobile/>
+            {userId && (
+              <NavLink 
+              to={"/generate"}
+              className={({ isActive }) =>
+                cn(
+                  "text-base text-neutral-700",
+                  isActive && "text-neutral-900 font-semibold"
+                )
+                }
+                >
+                  Take an Interview
+                </NavLink>
+            )}
+            </nav>
+      </SheetContent>
+    </Sheet>
   )
 }
 
